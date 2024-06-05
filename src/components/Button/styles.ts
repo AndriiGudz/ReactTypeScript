264033af18ff48175d56b5146d66e190f4f21035
Andrii Gudz <andrey.gudz@gmail.com>
2024-06-05 19:04:41 +0200

    Homeworks_08 update

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,6 +1,10 @@
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 
+interface StyledButtonProps {
+  disabled: boolean
+}
+
 export const ButtonCompanent = styled.button`
   width: 100%;
   height: 70px;
@@ -8,14 +12,15 @@ export const ButtonCompanent = styled.button`
   border: none;
   border-radius: 4px;
   opacity: 0px;
-  background-color: #1f27f5;
+  /* background-color: #1f27f5; */
+  background: ${({disabled}) => disabled ? 'grey' : '#1f27f5'};
   color: white;
   font-size: 20px;
   font-weight: 600;
   cursor: pointer;
 
-  &:disabled {
+  /* &:disabled {
     background-color: gray;
     cursor: auto;
-  }
+  } */
 `
