77fbc9eb215636f710013f3856a14a3a360327c2
Andrii Gudz <andrey.gudz@gmail.com>
2024-06-08 15:14:30 +0200

    Homeworks 11 update

--- src/homeworks/Homeworks11/Homeworks11.tsx
+++ src/homeworks/Homeworks11/Homeworks11.tsx
@@ -1,97 +1,10 @@
-import { useFormik } from 'formik'
-import * as Yup from 'yup'
-
-import Input from 'components/Input/Input'
-import { Homeworks11Component, EmployeeForm, TitleForm, TermErrors, TermsAccepted } from './styles'
-import { EmployeeFormValues, EMPLOYEE_FORM_NAMES } from './types'
-import Button from 'components/Button/Button'
+import EmployeeForm from 'components/EmployeeForm/EmployeeForm'
+import { Homeworks11Component } from './styles'
 
 function Homeworks11() {
-  const schema = Yup.object().shape({
-    [EMPLOYEE_FORM_NAMES.FIRST_NAME]: Yup.string()
-      .required('Имя является обязательным')
-      .min(2, 'Имя должно содержать минимум 2 символа')
-      .max(50, 'Имя должно содержать максимум 50 символов'),
-
-    [EMPLOYEE_FORM_NAMES.LAST_NAME]: Yup.string()
-      .required('Фамилия является обязательной')
-      .max(15, 'Фамилия должна содержать максимум 15 символов'),
-
-    [EMPLOYEE_FORM_NAMES.AGE]: Yup.number()
-      .required('Возраст является обязательным')
-      .min(18, 'Возраст должен быть не меньше 18')
-      .max(150, 'Возраст должен быть не больше 150'),
-
-    [EMPLOYEE_FORM_NAMES.JOB_TITLE]: Yup.string()
-      .max(30, 'Должность должна содержать максимум 30 символов')
-      .notRequired(),
-
-    [EMPLOYEE_FORM_NAMES.TERMS_ACCEPTED]: Yup.boolean()
-      .oneOf([true], 'Вы должны принять условия использования')
-      .required('Вы должны принять условия использования'),
-  })
-
-  const formik = useFormik({
-    initialValues: {
-      [EMPLOYEE_FORM_NAMES.FIRST_NAME]: '',
-      [EMPLOYEE_FORM_NAMES.LAST_NAME]: '',
-      [EMPLOYEE_FORM_NAMES.AGE]: '',
-      [EMPLOYEE_FORM_NAMES.JOB_TITLE]: '',
-      [EMPLOYEE_FORM_NAMES.TERMS_ACCEPTED]: false,
-    } as EmployeeFormValues,
-    validationSchema: schema,
-    onSubmit: (values) => {
-      console.log(values)
-    },
-  })
-
   return (
     <Homeworks11Component>
-      <EmployeeForm onSubmit={formik.handleSubmit}>
-        <TitleForm>Employee Form</TitleForm>
-        <Input
-          name={EMPLOYEE_FORM_NAMES.FIRST_NAME}
-          placeholder={'Имя'}
-          value={formik.values[EMPLOYEE_FORM_NAMES.FIRST_NAME]}
-          onChange={formik.handleChange}
-          error={formik.errors[EMPLOYEE_FORM_NAMES.FIRST_NAME]}
-        />
-        <Input
-          name={EMPLOYEE_FORM_NAMES.LAST_NAME}
-          placeholder={'Фамилия'}
-          value={formik.values[EMPLOYEE_FORM_NAMES.LAST_NAME]}
-          onChange={formik.handleChange}
-          error={formik.errors[EMPLOYEE_FORM_NAMES.LAST_NAME]}
-        />
-        <Input
-          name={EMPLOYEE_FORM_NAMES.AGE}
-          placeholder={'Возраст'}
-          value={formik.values[EMPLOYEE_FORM_NAMES.AGE]}
-          onChange={formik.handleChange}
-          error={formik.errors[EMPLOYEE_FORM_NAMES.AGE]}
-        />
-        <Input
-          name={EMPLOYEE_FORM_NAMES.JOB_TITLE}
-          placeholder={'Должность'}
-          value={formik.values[EMPLOYEE_FORM_NAMES.JOB_TITLE]}
-          onChange={formik.handleChange}
-          error={formik.errors[EMPLOYEE_FORM_NAMES.JOB_TITLE]}
-        />
-        <TermsAccepted>
-          <input
-            type="checkbox"
-            name={EMPLOYEE_FORM_NAMES.TERMS_ACCEPTED}
-            checked={formik.values[EMPLOYEE_FORM_NAMES.TERMS_ACCEPTED]}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          <label htmlFor={EMPLOYEE_FORM_NAMES.TERMS_ACCEPTED}>Принять правила использования</label>
-          {formik.touched[EMPLOYEE_FORM_NAMES.TERMS_ACCEPTED] && formik.errors[EMPLOYEE_FORM_NAMES.TERMS_ACCEPTED] && (
-            <TermErrors>{formik.errors[EMPLOYEE_FORM_NAMES.TERMS_ACCEPTED]}</TermErrors>
-          )}
-        </TermsAccepted>
-        <Button name="Создать" type="submit" />
-      </EmployeeForm>
+      <EmployeeForm />
     </Homeworks11Component>
   )
 }
--- src/homeworks/Homeworks11/styles.ts
+++ src/homeworks/Homeworks11/styles.ts
@@ -8,27 +8,3 @@ export const Homeworks11Component = styled.div`
   flex: 1;
   gap: 20px;
 `
-
-export const EmployeeForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 600px;
-  height: auto;
-  border: 1px solid #000;
-  border-radius: 4px;
-  padding: 40px 20px;
-  gap: 20px;
-`
-export const TitleForm = styled.h2`
-  display: flex;
-  align-items: flex-start;
-`
-export const TermErrors = styled.div`
-  color: red;
-`
-export const TermsAccepted = styled.div`
-  /* display: flex; */
-  /* flex-direction: column; */
-`
